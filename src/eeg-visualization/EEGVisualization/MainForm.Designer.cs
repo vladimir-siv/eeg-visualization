8c9559a9d5293c4c6c1e117afce01e740710ae0d
vladimir-siv <vladimir.sivcev@gmail.com>
2020-01-21 00:00:48 +0100

    Project Setup

---
+++ src/eeg-visualization/EEGVisualization/MainForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace EEGVisualization
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.GLControl = new SharpGL.OpenGLControl();
+			((System.ComponentModel.ISupportInitialize)(this.GLControl)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// GLControl
+			// 
+			this.GLControl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.GLControl.DrawFPS = false;
+			this.GLControl.FrameRate = 60;
+			this.GLControl.Location = new System.Drawing.Point(0, 0);
+			this.GLControl.Name = "GLControl";
+			this.GLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL4_4;
+			this.GLControl.RenderContextType = SharpGL.RenderContextType.NativeWindow;
+			this.GLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+			this.GLControl.Size = new System.Drawing.Size(800, 600);
+			this.GLControl.TabIndex = 0;
+			this.GLControl.OpenGLInitialized += new System.EventHandler(this.GLControl_OpenGLInitialized);
+			this.GLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.GLControl_OpenGLDraw);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 600);
+			this.Controls.Add(this.GLControl);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "MainForm";
+			this.ShowIcon = false;
+			this.Text = "EEG Visualization";
+			((System.ComponentModel.ISupportInitialize)(this.GLControl)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private SharpGL.OpenGLControl GLControl;
+	}
+}
+
---
+++ src/eeg-visualization/EEGVisualization/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EEGVisualization")]
+[assembly: AssemblyDescription("EEG visualization application.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NanoTechnologies")]
+[assembly: AssemblyProduct("EEGVisualization")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("NT™")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5f2ee4c2-0255-4b8c-a964-220e5095daa6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
